db885c5ce9e3c9b0c105c61a7d2fffe1ae5f0501
filipesousaist <filipe.miguel.sousa@tecnico.ulisboa.pt>
2019-04-03 01:08:58 +0100

    Reticências e objetos
    Reticências em vez de marquees + criação do objetos para localizações em vez da classe Location

---
+++ iGo/database.js
@@ -0,0 +1,45 @@
+const location_type_img =
+{
+  "monument": "img/Monument.png",
+  "museum": "img/Museum.png",
+  "shop": "img/shop.png",
+  "park": "img/park.png"
+}
+
+const locations =
+{
+  "Parque Eduardo VII":
+  {
+    title: "Parque Eduardo VII",
+    info: "Parque Eduardo VII é o maior parque na zona de Lisboa",
+    type: "park"
+  },
+
+  "Shopping Amoreiras":
+  {
+    title: "Shopping Amoreiras",
+    info: "Shopping Amoreiras é um Shopping bue grande",
+    type: "shop"
+  },
+
+	"Instituto Gulbenkian":
+  {
+    title: "Instituto Gulbenkian",
+    info: "Instituto Gulbenkian é bue fixe",
+    type: "museum"
+  },
+
+	"Padrão dos Descobrimentos":
+  {
+    title: "Padrão dos Descobrimentos",
+    info: "É bue velho, meu",
+    type: "monument"
+  },
+
+	"Torre de Belém":
+  {
+    title: "Torre de Belém",
+    info: "É bue velho tmb, meu",
+    type: "monument"
+  }
+}
--- iGo/script.js
+++ iGo/script.js
@@ -10,30 +10,12 @@ class Screen
 	}
 }
 
-class Location
-{
-	constructor(title, info, type)
-	{
-		this.title = title;
-		this.info = info;
-		this.type = type;
-	}
-}
-
 var current_screen;
 var power;
 
-var locations = [
-	new Location("Parque Eduardo VII", "Parque Eduardo VII é o maior parque na zona de Lisboa", "park"),
-	new Location("Shopping Amoreiras", "Shopping Amoreiras é um Shopping bue grande", "shop"),
-	new Location("Instituto Gulbenkian", "Instituto Gulbenkian é bue fixe", "museum"),
-	new Location("Padrão dos Descobrimentos", "É bue velho, meu", "monument"),
-	new Location("Torre de Belém", "É bue velho tmb, meu", "monument")
-]
-
 var screens =
-{	
-	//screen desligado
+{
+	// Ecrã desligado
 	"off": new Screen("off", null,
 		function()
 		{
@@ -42,13 +24,12 @@ var screens =
 			document.getElementById("iGo_logo").style.opacity = 0;
 		},
 		function()
-		{	
+		{
 			document.getElementById("home_button").onclick = function(){ change_screen("main_menu"); }
 			document.getElementById("back_button").onclick = go_back;
 		},
-		null
-	),
-	
+		null),
+
 	// Menu inicial
 	"main_menu": new Screen("main_menu", null,
 		function()
@@ -58,38 +39,40 @@ var screens =
 			this.properties["timeout"] = setInterval(update_clock, 1000);
 		},
 		function(){clearInterval(this.properties["timeout"]);},
-		{"clock_blink": false, "timeout": null}
-	),
-	
+		{"clock_blink": false, "timeout": null}),
+
 	// Ecrã das aplicações
 	"apps": new Screen("apps", "main_menu", null, null, null),
-	
+
 	// Ecrã da localização
 	"location": new Screen("location", "main_menu", null, null, null),
-	
+
 	// Ecrã das opções
 	"options": new Screen("options", "main_menu", null, null, null),
-	
+
 	// Ecrã do iGuide
-	"iGuide_main": new Screen("iGuide_main", "apps", 
+	"iGuide_main": new Screen("iGuide_main", "apps",
 	function()
 	{
 		let places_element = document.getElementById("iGuide_places_near_you");
-		let info_code = "<img class='info_icon' src='img/infoicon.png'>";		
-		for (let i = 0; i < locations.length; i ++){
-			places_element.innerHTML += "<li class='iGuide_place' onclick='changeInfoScreen(\""+ locations[i].title +"\", \""+ locations[i].info + "\", \"" + locations[i].type +"\");'><marquee direction='scroll'>" + locations[i].title + "</marquee>" + info_code + "</li>";
+		let info_img;
+		for (let l in locations)
+		{
+			info_img = "<img class='info_icon' src='img/infoicon.png' onclick='changeInfoScreen(\""+ l +"\");'>";
+			places_element.innerHTML += "<li class='iGuide_place_frame'>" + "<div class='iGuide_place'>" + locations[l].title + "</div>" + info_img + "</li>";
 		}
-	}, function(){document.getElementById("iGuide_places_near_you").innerHTML = "";}, null),
+	},
+	function(){document.getElementById("iGuide_places_near_you").innerHTML = "";}, null),
 
 	// Ecrã do iGuide_info
 	"iGuide_info": new Screen("iGuide_info", "iGuide_main", null, null, null),
-	
+
 	// Ecrã do iWay
 	"iWay_main": new Screen("iWay_main", "apps", null, null, null),
-	
+
 	// Ecrã do iGroup
 	"iGroup_main": new Screen("iGroup_main", "apps", null, null, null),
-	
+
 	// Ecrã de erro
 	"error_screen": new Screen("error_screen", null,
 		function(){this.parent_id = current_screen.id;}, null, null)
@@ -108,7 +91,7 @@ function init()
 function replace_element(old_id, new_id)
 {
 	document.getElementById(new_id).style.display = "block";
-	
+
 	if (new_id == "error_screen")
 		fadein(new_id,0.3);
 	else
@@ -124,7 +107,7 @@ function change_screen(new_screen_id)
 			current_screen.on_exit();
 
 		replace_element(current_screen.id, new_screen_id);
-		
+
 		let new_screen = screens[new_screen_id];
 		if (new_screen.on_load != null)
 			new_screen.on_load();
@@ -134,7 +117,7 @@ function change_screen(new_screen_id)
 }
 
 function go_back()
-{	
+{
 	var parent_screen_id = current_screen.parent_id;
 	if (parent_screen_id != null)
 		change_screen(parent_screen_id);
@@ -159,48 +142,36 @@ function update_clock()
 	document.getElementById("clock").innerHTML = hours + sep + minutes;
 }
 
-function fadein(id,seconds)
+function fadein(id, seconds)
 {
 	document.getElementById(id).style.animation = "fade "+seconds+"s";
 	document.getElementById(id).style.opacity = "1";
-	
 }
 
-function changeInfoScreen(title, info, icon){
-	var info_title = document.getElementById("iGuide_info_title");
-	info_title.innerHTML = title;
-	document.getElementById("iGuide_info_container").innerHTML = info;
-	let icondiv = document.getElementById("iGuide_info_icon");
-	switch(icon){
-		case "monument":
-			icondiv.src = "img/Monument.png";
-			break;
-		case "museum":
-			icondiv.src = "img/Museum.png";
-			break;
-		case "shop":
-			icondiv.src = "img/shop.png";
-			break;
-		case "park":
-			icondiv.src = "img/park.png";
-			break;
-	}
+function changeInfoScreen(location_name)
+{
+	let location = locations[location_name];
+	document.getElementById("iGuide_info_title").innerHTML = location.title;
+	document.getElementById("iGuide_info_container").innerHTML = location.info;
+	document.getElementById("iGuide_info_icon").src = location_type_img[location.type];
 	change_screen("iGuide_info");
 }
 
-function turn_off_on(){
-	switch(power){
-		case 0:
-			document.getElementById("iGo_logo").style.animation = "fade_1 1.5s";
-			setTimeout(function(){
-				power = 1;
-				change_screen("main_menu");
-			},1600);
-			break;
-		case 1:
-			document.getElementById("iGo_logo").style.animation = "";
-			power = 0;
-			change_screen("off");
-			break;
+function turn_off_on()
+{
+	if (power)
+	{
+		document.getElementById("iGo_logo").style.animation = "";
+		power = 0;
+		change_screen("off");
+	}
+	else
+	{
+		document.getElementById("iGo_logo").style.animation = "fade 1.5s";
+		setTimeout(function()
+		{
+			power = 1;
+			change_screen("main_menu");
+		}, 1600);
 	}
 }
